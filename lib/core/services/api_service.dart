34968adb2b32521c8a427ee67a788628de561bcb
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2023-12-12 12:42:49 +0300

    add downloadFile to api_service

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
+import 'dart:io';
 
-import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-import 'package:logger/logger.dart';
 import 'package:elresala/core/constants/app_api_routes.dart';
+import 'package:elresala/core/errors/exception.dart';
 import 'package:elresala/core/helpers/get_exception_from_status_code.dart';
 import 'package:elresala/core/helpers/header.dart';
 import 'package:elresala/core/helpers/network_info.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ApiService extends GetxService {
   final http.Client client;
@@ -31,9 +34,9 @@ class ApiService extends GetxService {
   }) async {
     try {
       Get.find<Logger>().i('Start post `$subUrl` |ApiServiceImpl| data: $data');
-      // if (!(await networkInfo.isConnected)) {
-      //   throw OfflineException();
-      // }
+      if (!(await networkInfo.isConnected)) {
+        throw OfflineException();
+      }
       final response = await client.post(
         Uri.http(
           AppApiRoutes.baseUrl,
@@ -59,9 +62,9 @@ class ApiService extends GetxService {
   }) async {
     try {
       Get.find<Logger>().i('Start get `$subUrl` |ApiServiceImpl| parameters: $parameters');
-      // if (!(await networkInfo.isConnected)) {
-      //   throw OfflineException();
-      // }
+      if (!(await networkInfo.isConnected)) {
+        throw OfflineException();
+      }
       parameters?.removeWhere((key, value) => value == 'null');
       final response = await client.get(
         Uri.http(
@@ -76,6 +79,50 @@ class ApiService extends GetxService {
       return Future.value((json.decode(response.body)));
     } catch (e) {
       Get.find<Logger>().e('End get `$subUrl` |ApiServiceImpl| Exception: ${e.runtimeType}');
+      rethrow;
+    }
+  }
+
+  Future<String> downloadFile({
+    required String url,
+    required String subPath,
+    required Function(double) onProgress,
+  }) async {
+    try {
+      Get.find<Logger>().i('Start downloadFile `$url` |ApiServiceImpl| subPath: $subPath');
+      if (!(await networkInfo.isConnected)) {
+        throw OfflineException();
+      }
+
+      final http.Request request = http.Request('GET', Uri.parse(url));
+      var response = await client.send(request);
+      final contentLength = response.headers['content-length'];
+      final totalBytes = contentLength != null ? int.parse(contentLength) : 0;
+      int receivedBytes = 0;
+
+      final appDocumentDirectory = await getApplicationDocumentsDirectory();
+      final file = File('${appDocumentDirectory.path}/$subPath');
+      final sink = file.openWrite();
+
+      try {
+        await response.stream.forEach(
+          (List<int> chunk) {
+            sink.add(chunk);
+            receivedBytes += chunk.length;
+            onProgress(receivedBytes / totalBytes);
+          },
+        );
+      } catch (error) {
+        Get.find<Logger>().e('Error during file download: $error');
+      } finally {
+        await sink.flush();
+        await sink.close();
+      }
+
+      Get.find<Logger>().f('End downloadFile `$url` |ApiServiceImpl|');
+      return Future.value(file.path);
+    } catch (e) {
+      Get.find<Logger>().e('End downloadFile `$url` |ApiServiceImpl| Exception: ${e.runtimeType}');
       rethrow;
     }
   }
