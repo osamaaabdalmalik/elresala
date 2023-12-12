06920e0ef204e9a7685f7c8d2f90750c204f510a
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2023-12-12 23:36:17 +0300

    merge Mohamad Adib Tawil changes in single branch

--- lib/core/styles/text_styles.dart
+++ lib/core/styles/text_styles.dart
@@ -1,23 +1,20 @@
-import 'package:alresala/constants.dart';
+import 'package:elresala/core/constants/app_colors.dart';
 import 'package:flutter/cupertino.dart';
-
 
 abstract class Styles {
   static const textStyleQuranPageNumber = TextStyle(
-              color: kPrimaryColor,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            );
+    color: AppColors.kPrimaryColor,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
   static const textStyleAya = TextStyle(
-              color: kGoldenColor,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            );
- static const textStyleAyaLanguage = TextStyle(
-              color: kBlackColor,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            );
+    color: AppColors.kGoldenColor,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+  static const textStyleAyaLanguage = TextStyle(
+    color: AppColors.kBlackColor,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
 }
-
- 
