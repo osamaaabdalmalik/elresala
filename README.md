8eaa907077b74f2e77331a23b96fbd8be69b1e73
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2023-12-13 00:32:51 +0300

    fix assets paths and edit readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# elresala
+# Elresala
 
 A new Flutter project.
 
@@ -15,19 +15,3 @@ For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 
-get: ^4.6.5
-dartz: ^0.10.1
-internet_connection_checker: ^1.0.0+1
-http: ^1.1.0
-shared_preferences: ^2.2.2
-cached_network_image: ^3.2.3
-flutter_svg: ^2.0.7
-logger: ^2.0.2
-flutter_screenutil: ^5.9.0
-multi_select_flutter: ^4.1.3
-dropdown_button2: ^2.3.9
-loading_animation_widget: ^1.2.0+4
-shimmer: ^3.0.0
-flutter_easyloading: ^3.0.5
-flutter_inset_box_shadow: ^1.0.8
-google_fonts: ^6.1.0
--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -6,34 +6,34 @@ abstract class AppAssets {
   static const String image = "$rootImages/1.png";
 
   // SVG
-  static const String rootSVG = "assets/svg/";
+  static const String rootSVG = "assets/svg";
 
   //
   static const String rootSVGIcon = "$rootSVG/icons";
   static const String profileDefault = "$rootSVG/profile_default.svg";
   static const String categoriesIconSvg = "$rootSVGIcon/categories.svg";
+  static const String kBasmalaImage = '$rootImages/basmala.png';
 
   static const String kCopyIcon = '$rootSVGIcon/copy.svg';
-static const String kEllipseBallProgress = '$rootSVGIcon/Ellipse_ball_progress.svg';
-static const String kLineIcon = '$rootSVGIcon/Line 57.svg';
-static const String kBasmalaImage = 'assets/images/basmala.png';
-static const String kPauseIcon = '$rootSVGIcon/pause_11.svg';
-static const String kPlayIcon = '$rootSVGIcon/Play_fill.svg';
-static const String kPreviosPlayIcon = '$rootSVGIcon/previos_play.svg';
-static const String kShareIcon = '$rootSVGIcon/share.svg';
-static const String kSoudisIcon = '$rootSVGIcon/soudis.png';
-static const String kTranslateIcon = '$rootSVGIcon/translate.svg';
-static const String kVectorUpIcon = '$rootSVGIcon/Vector 9.svg';
-static const String kEyeViewFillIcon = '$rootSVGIcon/View_fill.svg';
-static const String kStepPalyIcon = '$rootSVGIcon/step_play.svg';
-static const String kBackIcon = '$rootSVGIcon/kBackIcon.svg';
-static const String kZaghrafaIcon = '$rootSVGIcon/zaghrafa.png';
-static const String kBookmarkFillIcon = '$rootSVGIcon/Bookmark_fill.svg';
-static const String kBookmarkIcon = '$rootSVGIcon/Bookmark.svg';
-static const String kSearchIcon = '$rootSVGIcon/Search.svg';
-static const String kDownloadIcon = '$rootSVGIcon/Download.svg';
+  static const String kEllipseBallProgress = '$rootSVGIcon/Ellipse_ball_progress.svg';
+  static const String kLineIcon = '$rootSVGIcon/Line 57.svg';
+  static const String kPauseIcon = '$rootSVGIcon/pause_11.svg';
+  static const String kPlayIcon = '$rootSVGIcon/Play_fill.svg';
+  static const String kPreviosPlayIcon = '$rootSVGIcon/previos_play.svg';
+  static const String kShareIcon = '$rootSVGIcon/share.svg';
+  static const String kTranslateIcon = '$rootSVGIcon/translate.svg';
+  static const String kVectorUpIcon = '$rootSVGIcon/Vector 9.svg';
+  static const String kEyeViewFillIcon = '$rootSVGIcon/View_fill.svg';
+  static const String kStepPalyIcon = '$rootSVGIcon/step_play.svg';
+  static const String kBackIcon = '$rootSVGIcon/kBackIcon.svg';
+  static const String kZaghrafaIcon = '$rootSVGIcon/zaghrafa.png';
+  static const String kBookmarkFillIcon = '$rootSVGIcon/Bookmark_fill.svg';
+  static const String kBookmarkIcon = '$rootSVGIcon/Bookmark.svg';
+  static const String kSearchIcon = '$rootSVGIcon/Search.svg';
+  static const String kDownloadIcon = '$rootSVGIcon/Download.svg';
 
   //
   static const String rootSvgImages = 'assets/svg/images';
+  static const String kSoudisIcon = '$rootSvgImages/soudis.png';
   static const String choseImage = '$rootSvgImages/chose_image.svg';
 }
