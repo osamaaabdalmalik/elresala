4d359a37ec0a9758dc63efdb06d4b78429d11d55
Mohamad Adib Tawil <https://github.com/mohamadAdibTawil/notes_app.git>
2023-12-12 22:28:03 +0300

    styles

---
+++ lib/core/styles/text_styles.dart
@@ -0,0 +1,23 @@
+import 'package:alresala/constants.dart';
+import 'package:flutter/cupertino.dart';
+
+
+abstract class Styles {
+  static const textStyleQuranPageNumber = TextStyle(
+              color: kPrimaryColor,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            );
+  static const textStyleAya = TextStyle(
+              color: kGoldenColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            );
+ static const textStyleAyaLanguage = TextStyle(
+              color: kBlackColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            );
+}
+
+ 
