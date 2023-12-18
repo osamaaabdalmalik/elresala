912565f27ea939cb8de6222ac3e7f1c197421660
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2023-12-18 10:19:44 +0300

    add home screen

--- lib/features/main/presentation/controller/main_controller.dart
+++ lib/features/main/presentation/controller/main_controller.dart
@@ -1,3 +1,6 @@
+import 'package:elresala/core/constants/app_assets.dart';
+import 'package:elresala/core/constants/app_pages_routes.dart';
+import 'package:elresala/features/main/domain/entities/home_card_data.dart';
 import 'package:elresala/features/main/domain/usecases/download_files_use_case.dart';
 import 'package:get/get.dart';
 import 'package:logger/logger.dart';
@@ -5,6 +8,57 @@ import 'package:logger/logger.dart';
 class MainController extends GetxController {
   double progress = 0;
   int totalFiles = 0, downloadedFiles = 0;
+
+  List<HomeCardData> homeCardsData = [
+    HomeCardData(
+      title: "Quran",
+      description: "Learn , Read all the Quran",
+      iconPath: AppAssets.quran,
+      targetScreen: AppPagesRoutes.mainScreen,
+    ),
+    HomeCardData(
+      title: "Calender",
+      description: "Data,All holidays and special occasions for Muslims",
+      iconPath: AppAssets.calender,
+      targetScreen: AppPagesRoutes.mainScreen,
+    ),
+    HomeCardData(
+      title: "Prayer",
+      description: "Learn how to pray,Qiblah",
+      iconPath: AppAssets.prayer,
+      targetScreen: AppPagesRoutes.mainScreen,
+    ),
+    HomeCardData(
+      title: "Dua",
+      description: "Learn how to pray,Qiblah",
+      iconPath: AppAssets.prayer,
+      targetScreen: AppPagesRoutes.mainScreen,
+    ),
+    HomeCardData(
+      title: "Azkar",
+      description: "Learn how to pray,Qiblah",
+      iconPath: AppAssets.azkar,
+      targetScreen: AppPagesRoutes.mainScreen,
+    ),
+    HomeCardData(
+      title: "Hadith",
+      description: "Learn how to pray,Qiblah",
+      iconPath: AppAssets.hadith,
+      targetScreen: AppPagesRoutes.mainScreen,
+    ),
+    HomeCardData(
+      title: "Advanced Learning",
+      description: "Learn how to pray,Qiblah",
+      iconPath: AppAssets.hadith,
+      targetScreen: AppPagesRoutes.mainScreen,
+    ),
+    HomeCardData(
+      title: "Courses for Muslim",
+      description: "Learn how to pray,Qiblah",
+      iconPath: AppAssets.hadith,
+      targetScreen: AppPagesRoutes.mainScreen,
+    ),
+  ];
 
   @override
   void onInit() async {
--- lib/features/main/presentation/screens/languages_screen.dart
+++ lib/features/main/presentation/screens/languages_screen.dart
@@ -1,13 +1,13 @@
 import 'package:elresala/core/constants/app_assets.dart';
 import 'package:elresala/core/constants/app_colors.dart';
 import 'package:elresala/features/main/presentation/controller/main_controller.dart';
-import 'package:elresala/features/main/presentation/widgets/home_card.dart';
+import 'package:elresala/features/main/presentation/widgets/downlaod_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
-class MainScreen extends GetView<MainController> {
-  const MainScreen({super.key});
+class LanguagesScreen extends GetView<MainController> {
+  const LanguagesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +19,14 @@ class MainScreen extends GetView<MainController> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: ListView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 const Text(
-                  "Home",
+                  "Languages",
                   style: TextStyle(
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
@@ -35,20 +36,25 @@ class MainScreen extends GetView<MainController> {
                 SvgPicture.asset(AppAssets.logo),
               ],
             ),
-            GetBuilder<MainController>(
-              builder: (controller) => ListView.separated(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                padding: const EdgeInsets.symmetric(vertical: 30),
-                itemCount: controller.homeCardsData.length,
-                itemBuilder: (context, index) => HomeCard(
-                  homeCardData: controller.homeCardsData[index],
-                ),
-                separatorBuilder: (BuildContext context, int index) => const SizedBox(
-                  height: 15,
-                ),
-              ),
+            const SizedBox(
+              height: 30,
             ),
+            GetBuilder<MainController>(
+              builder: (controller) => ListView(
+                shrinkWrap: true,
+                children: [
+                  DownloadCard(
+                    title: "Spanish",
+                    progress: controller.progress,
+                    downloadedCount: controller.downloadedFiles,
+                    totalCount: controller.totalFiles,
+                    onDownloadTab: () async {
+                      await controller.downloadFiles();
+                    },
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
