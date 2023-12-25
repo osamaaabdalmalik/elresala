a01dfdbb86a49bb3a5c48642494f54958c524fd2
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2023-12-25 23:14:40 +0300

    add file paths to `app_keys`

--- lib/core/constants/app_keys.dart
+++ lib/core/constants/app_keys.dart
@@ -1,9 +1,14 @@
 class AppKeys {
   static const String lang = 'passId';
 
-  // file paths
   static const String languages = 'languages';
   static const String currentLanguage = 'currentLanguage';
+
+  // file paths
   static const String quran = 'quran.json';
   static const String hadith = 'hadith.json';
+  static const String azkarDua = 'azkar_dua.json';
+  static const String muslims = 'muslims.json';
+  static const String nonMuslims = 'non_muslims.json';
+  static const String sites = 'sites.json';
 }
