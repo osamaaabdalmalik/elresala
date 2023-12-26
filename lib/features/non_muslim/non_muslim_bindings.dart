6194938f5c758bb7f86bd1744346e895b79c1be2
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2023-12-26 08:44:28 +0300

    add non muslim feature

---
+++ lib/features/non_muslim/non_muslim_bindings.dart
@@ -0,0 +1,29 @@
+import 'package:elresala/features/non_muslim/data/data_sources/non_muslim_local_data_source.dart';
+import 'package:elresala/features/non_muslim/data/data_sources/non_muslim_remote_data_source.dart';
+import 'package:elresala/features/non_muslim/data/repository/non_muslim_repo_impl.dart';
+import 'package:elresala/features/non_muslim/domain/repository/non_muslim_repo.dart';
+import 'package:elresala/features/non_muslim/presentation/controller/non_muslim_controller.dart';
+import 'package:get/get.dart';
+
+class NonMuslimBindings extends Bindings {
+  @override
+  dependencies() async {
+    Get.put<NonMuslimRemoteDataSource>(
+      NonMuslimRemoteDataSourceImpl(apiService: Get.find()),
+    );
+    Get.put<NonMuslimLocalDataSource>(
+      NonMuslimLocalDataSourceImpl(
+        sharedPreferencesService: Get.find(),
+        firebaseStorageService: Get.find(),
+      ),
+    );
+    Get.put<NonMuslimRepo>(
+      NonMuslimRepoImpl(
+        nonMuslimLocalDataSource: Get.find(),
+        nonMuslimRemoteDataSource: Get.find(),
+      ),
+    );
+
+    Get.put(NonMuslimController());
+  }
+}
