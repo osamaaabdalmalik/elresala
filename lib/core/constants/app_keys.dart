b57c1ac1a51029e275c82286226a1df988b6cfc4
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-01-27 19:21:51 +0300

    add key for doaa file

--- lib/core/constants/app_keys.dart
+++ lib/core/constants/app_keys.dart
@@ -8,6 +8,7 @@ class AppKeys {
   static const String quran = 'quran.json';
   static const String hadith = 'hadith.json';
   static const String azkarDua = 'azkar_dua.json';
+  static const String doaa = 'doaa.json';
   static const String muslims = 'muslims.json';
   static const String nonMuslims = 'non_muslims.json';
   static const String sites = 'sites.json';
