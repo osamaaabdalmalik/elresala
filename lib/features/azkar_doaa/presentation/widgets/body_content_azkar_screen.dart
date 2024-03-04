6233dede15dd5572aa4b5242a6d5ece454acefdd
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-03-04 21:17:15 +0300

    fix current page of azkar

--- lib/features/azkar_doaa/presentation/widgets/body_content_azkar_screen.dart
+++ lib/features/azkar_doaa/presentation/widgets/body_content_azkar_screen.dart
@@ -18,154 +18,155 @@ class BodyContentAzkarScreen extends GetView<AzkarDoaaController> {
   @override
   Widget build(BuildContext context) {
     List<ZikrModel> azkar = Get.arguments["data"];
-    return SliverToBoxAdapter(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 12),
-            SizedBox(
-              height: Get.height * 0.6,
-              child: PageView.builder(
-                controller: controller.pageController,
-                itemCount: azkar.length,
-                onPageChanged: controller.onPageChanged,
-                itemBuilder: (context, index) {
-                  ZikrModel zikr = azkar[index];
-                  return SingleChildScrollView(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.w),
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: AppColors.transparent,
-                            backgroundImage:
-                                const AssetImage(AppAssets.kZaghrafaIcon),
-                            child: Text(
-                              (index + 1).toString(),
-                              style: Styles.textStyle14Golden,
+    return PopScope(
+      onPopInvoked: (didPop) {
+        controller.currentPage.value = 0;
+      },
+      child: SliverToBoxAdapter(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 12),
+              SizedBox(
+                height: Get.height * 0.6,
+                child: PageView.builder(
+                  controller: controller.pageController,
+                  itemCount: azkar.length,
+                  onPageChanged: controller.onPageChanged,
+                  itemBuilder: (context, index) {
+                    ZikrModel zikr = azkar[index];
+                    return SingleChildScrollView(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.w),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: AppColors.transparent,
+                              backgroundImage: const AssetImage(AppAssets.kZaghrafaIcon),
+                              child: Text(
+                                (index + 1).toString(),
+                                style: Styles.textStyle14Golden,
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 20.h),
-                          HadithArabicContainer(
-                            hadithArabic: zikr.arabic ?? "",
-                          ),
-                          const SizedBox(height: 12),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Container(
-                                  padding: const EdgeInsets.only(left: 12),
-                                  constraints: BoxConstraints(
-                                    maxWidth:
-                                        MediaQuery.sizeOf(context).width - 70,
-                                  ),
-                                  child: Text(
-                                    zikr.espaniol ?? "",
-                                    style: Styles.textStyle18Black,
-                                    textDirection: TextDirection.ltr,
-                                  ),
-                                ),
-                                InkWell(
-                                  onTap: () async {
-                                    var data = ClipboardData(
-                                        text: zikr.espaniol ?? "");
-                                    await Clipboard.setData(data);
-                                    EasyLoaderService.showToast(
-                                        message: "Copied");
-                                  },
-                                  child: SvgPicture.asset(
-                                    AppAssets.kCopyIcon,
-                                  ),
-                                ),
-                              ],
+                            SizedBox(height: 20.h),
+                            HadithArabicContainer(
+                              hadithArabic: zikr.arabic ?? "",
                             ),
-                          ),
-                        ],
+                            const SizedBox(height: 12),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.only(left: 12),
+                                    constraints: BoxConstraints(
+                                      maxWidth: MediaQuery.sizeOf(context).width - 70,
+                                    ),
+                                    child: Text(
+                                      zikr.espaniol ?? "",
+                                      style: Styles.textStyle18Black,
+                                      textDirection: TextDirection.ltr,
+                                    ),
+                                  ),
+                                  InkWell(
+                                    onTap: () async {
+                                      var data = ClipboardData(text: zikr.espaniol ?? "");
+                                      await Clipboard.setData(data);
+                                      EasyLoaderService.showToast(message: "Copied");
+                                    },
+                                    child: SvgPicture.asset(
+                                      AppAssets.kCopyIcon,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 80.h),
+              Container(
+                width: Get.width,
+                height: 50.sp,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: AppColors.kGoldenColor,
+                  borderRadius: BorderRadius.circular(15.sp),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Obx(
+                      () => Text(
+                        '${azkar[controller.currentPage.value].repeatNumber.toString()} / ',
+                        style: const TextStyle(color: AppColors.black),
                       ),
                     ),
-                  );
-                },
+                    const Text(
+                      "1",
+                      style: TextStyle(color: AppColors.white),
+                    ),
+                    const Icon(Icons.repeat, color: AppColors.black),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 80.h),
-            Container(
-              width: Get.width,
-              height: 50.sp,
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: AppColors.kGoldenColor,
-                borderRadius: BorderRadius.circular(15.sp),
-              ),
-              child: Row(
+              SizedBox(height: 20.h),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Obx(
-                    () => Text(
-                      '${azkar[controller.currentPage.value].repeatNumber.toString()} / ',
-                      style: const TextStyle(color: AppColors.black),
+                  navigateToPage(
+                    Icons.arrow_back_ios,
+                    controller.goToPreviousPage,
+                  ),
+                  SizedBox(width: 10.w),
+                  Container(
+                    padding: EdgeInsets.all(10.sp),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.sp),
+                      color: AppColors.kGreenColor,
                     ),
-                  ),
-                  const Text(
-                    "1",
-                    style: TextStyle(color: AppColors.white),
-                  ),
-                  const Icon(Icons.repeat, color: AppColors.black),
-                ],
-              ),
-            ),
-            SizedBox(height: 20.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                navigateToPage(
-                  Icons.arrow_back_ios,
-                  controller.goToPreviousPage,
-                ),
-                SizedBox(width: 10.w),
-                Container(
-                  padding: EdgeInsets.all(10.sp),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.sp),
-                    color: AppColors.kGreenColor,
-                  ),
-                  child: Row(
-                    children: [
-                      Text(
-                        azkar.length.toString(),
-                        style: TextStyle(
-                          color: AppColors.kGoldenColor,
-                          fontSize: 20.sp,
+                    child: Row(
+                      children: [
+                        Text(
+                          azkar.length.toString(),
+                          style: TextStyle(
+                            color: AppColors.kGoldenColor,
+                            fontSize: 20.sp,
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 10.w),
-                      Text(
-                        'of',
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                        ),
-                      ),
-                      SizedBox(width: 10.w),
-                      Obx(
-                        () => Text(
-                          (controller.currentPage.value + 1).toString(),
+                        SizedBox(width: 10.w),
+                        Text(
+                          'of',
                           style: TextStyle(
                             fontSize: 20.sp,
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(width: 10.w),
+                        Obx(
+                          () => Text(
+                            (controller.currentPage.value + 1).toString(),
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(width: 10.w),
-                navigateToPage(
-                  Icons.arrow_forward_ios,
-                  () => controller.goToNextPage(azkar),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(width: 10.w),
+                  navigateToPage(
+                    Icons.arrow_forward_ios,
+                    () => controller.goToNextPage(azkar),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
